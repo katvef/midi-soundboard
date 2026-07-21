6d58e0ce5e56ba7a8a02c74ae98d8c763a505ebb
Katve <kati@katvef.fi>
2026-07-22 02:46:43 +0300

    Basic config deserializer

--- intermediary.cpp
+++ intermediary.cpp
@@ -21,6 +21,66 @@ std::map<std::string, int> SoundBindings;
 ConfigurationVariables* Configs = new ConfigurationVariables();
 // #endif
 
+inline std::string Config::getConfigKey(const std::string& line)
+{
+	size_t start = line.find_first_not_of(" \t");
+	size_t end   = line.find_first_of(" \t=", start);
+	return line.substr(start, end);
+}
+
+inline std::string Config::getConfigValue(const std::string& line)
+{
+	size_t start = line.find_first_not_of(" \t", line.find('=') + 1);
+	size_t end   = line.find_last_not_of(" \t");
+	return line.substr(start, end);
+}
+
+inline bool Config::isCommentOrEmpty(const std::string_view& line)
+{
+	size_t start = line.find_first_not_of(" \t");
+	return start == std::string::npos || line[start] == '#';
+}
+
+void Config::deserialize()
+{
+	std::ifstream C;
+	C.open(ConfigPath);
+	std::string line;
+	while (getline(C, line)) {
+		try {
+			if (!isCommentOrEmpty(line)) {
+				const ConfigKeys key   = c_fromString(getConfigKey(line));
+				std::string      value = getConfigValue(line);
+
+				switch (c_getType(key)) {
+				case STRING:
+					config_keys[key] = value;
+					std::cout << "String\n";
+					break;
+				case INT:
+					config_keys[key] = value.find_first_not_of("1234567890") == std::string::npos ? std::stoi(value) : 0;
+					std::cout << "Int\n";
+					break;
+				}
+			}
+		} catch (const std::string e) {
+			std::cerr << e << '\n' << '\n';
+		}
+	}
+};
+
+Config::ConfigValue Config::getValue(ConfigKeys key)
+{
+	if (key >= ConfigKeys::LENGTH) { throw "Invalid key"; }
+	return config_keys[key];
+}
+
+Config::Config()
+{
+	config_keys.resize(ConfigKeys::LENGTH);
+	deserialize();
+}
+
 void initializeConfigFile()
 {
 	// std::ofstream ConfigFile;
--- intermediary.hpp
+++ intermediary.hpp
@@ -32,6 +32,64 @@ struct ConfigurationVariables
 extern struct ConfigurationVariables* Configs;
 // #endif
 
+class Config
+{
+ public:
+	enum ConfigTypes { STRING, INT };
+	enum ConfigKeys { MicrophoneOutput, MuteSoundboard, PlayLastSound, PlaybackOutput, MidiPortName, LENGTH };
+
+	static inline constexpr int c_getType(ConfigKeys key)
+	{
+		switch (key) {
+		case MicrophoneOutput: return STRING;
+		case PlaybackOutput:   return INT;
+		case MuteSoundboard:   return INT;
+		case PlayLastSound:    return INT;
+		case MidiPortName:     return STRING;
+		default:               throw "Key not found";
+		}
+	}
+
+	static inline constexpr std::string c_toString(ConfigKeys key)
+	{
+		switch (key) {
+		case MicrophoneOutput: return "MicrophoneOutput";
+		case MuteSoundboard:   return "MuteSoundboard";
+		case PlaybackOutput:   return "PlaybackOutput";
+		case PlayLastSound:    return "PlayLastSound";
+		case MidiPortName:     return "MidiPortName";
+		default:               throw "Key not found";
+		}
+	}
+
+	static inline constexpr ConfigKeys c_fromString(std::string key)
+	{
+		if (key == "MicrophoneOutput") { return ConfigKeys::MicrophoneOutput; }
+		if (key == "PlaybackOutput") { return ConfigKeys::PlaybackOutput; }
+		if (key == "MuteSoundboard") { return ConfigKeys::MuteSoundboard; }
+		if (key == "MidiPortName") { return ConfigKeys::MidiPortName; }
+		if (key == "PlayLastSound") { return ConfigKeys::PlayLastSound; }
+		throw "Invalid config key " + key;
+	}
+
+ private:
+	typedef std::variant<std::string, int> ConfigValue;
+
+	std::vector<ConfigValue> config_keys;
+
+	inline std::string getConfigKey(const std::string& line);
+
+	inline std::string getConfigValue(const std::string& line);
+
+	inline bool isCommentOrEmpty(const std::string_view& line);
+
+	void deserialize();
+
+ public:
+	ConfigValue getValue(ConfigKeys key);
+
+	Config();
+};
 // int getOutputDevices();
 
 int changeConfig(std::string ValueToChange, std::string NewValue);
